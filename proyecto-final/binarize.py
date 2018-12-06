b6267d6c74614f31b1e066d6ab13bf49b9d16382
arturomf94 <arturomf94@gmail.com>
2018-12-06 10:32:55 -0600

    Finish binarization and resizing

--- proyecto-final/binarize.py
+++ proyecto-final/binarize.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import cv2
 import os.path
 import glob
@@ -10,12 +11,23 @@ data_path = os.path.join(folder_name,'*g')
 g_files = glob.glob(data_path)
 files = g_files + G_files
 
+# Parameters for image size:
+
+width_objective = 100
+
 for image_name in files:
-    print(image_name)
+    # Read
     image = cv2.imread(image_name)
-    small = cv2.resize(image, (0,0), fx=0.2, fy=0.2)
+    # Resize
+    ratio = width_objective / image.shape[1]
+    dim = (width_objective, int(image.shape[0] * ratio))
+    height, width = image.shape[:2]
+    small = image
+    if width_objective < width:
+        small = cv2.resize(small, dim)
+    # To grayscale
     gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+    # Binarize
     ret, threshold_image = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
     binarized_image_name = image_name.replace(folder_name, destiny_folder_name)
-    print(binarized_image_name)
     cv2.imwrite(binarized_image_name, threshold_image)
